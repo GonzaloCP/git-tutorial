f550c4b7ed92e6d69b6ff60e9e0d1ebe9c433266
Ismael Cerda <gonzalo.cerda.xd@gmail.com>
2022-07-14 17:00:53 -0400

    Integración de Handlebars motor de plantilla

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
 
 //Iniciación
 const app = express();
 
 //Settings
 app.set('port', process.env.PORT || 8000);
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 //Rutas
 app.use(require('./routes/index'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('tu mama me gusta mucho dale besitos de mi parte');
+    res.render('index');
 });
 
 module.exports = router;
